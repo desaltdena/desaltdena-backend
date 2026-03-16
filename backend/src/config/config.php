282cf6a8edc1b7dcb89c01634518ab766843c0e3
desaltdena <desaltdena@gmail.com>
2026-03-16 23:29:33 +0700

    Update config.php

--- backend/src/config/config.php
+++ backend/src/config/config.php
@@ -27,7 +27,12 @@ class Connect extends PDO {
 }
 
 // ตั้งค่า Google Auth (ถ้าไม่ได้ใช้ ให้ปล่อยว่างไว้แบบนี้ครับ)
-define('GOOGLE_CLIENT_ID', '');
-define('GOOGLE_CLIENT_SECRET', '');
-define('GOOGLE_REDIRECT_URI', '');
+// define('GOOGLE_CLIENT_ID', '');
+// define('GOOGLE_CLIENT_SECRET', '');
+// define('GOOGLE_REDIRECT_URI', '');
+    define('GOOGLE_CLIENT_ID', getenv('GOOGLE_CLIENT_ID') ?: '');
+    define('GOOGLE_CLIENT_SECRET', getenv('GOOGLE_CLIENT_SECRET') ?: '');
+
+    $redirectUri = getenv('GOOGLE_REDIRECT_URI');
+    define('GOOGLE_REDIRECT_URI', $redirectUri);
 ?>
